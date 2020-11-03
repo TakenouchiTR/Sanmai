80077d3f491dc95721ec2978bd29e530773a57c2
Shawn Carter <totakenouchi@outlook.com>
2020-11-03 13:15:01 -0500

    Initial Commit

---
+++ FinalProject/Door.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    public class Door
+    {
+        public const int DOOR_WIDTH = 21;
+        public const int DOOR_HEIGHT = 9;
+
+        private int x, y;
+        private int frame;
+        private int number;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Number { get => number; set => number = value; }
+        public int Frame { get => frame; set => frame = value; }
+
+        private readonly string[] Display = new string[]
+        {
+            "|\\        |        /|",
+            "| \\       |       / |",
+            "|  \\      |      /  |",
+            "|   \\     |     /   |",
+            "|    >         <    |",
+            "|   /     |     \\   |",
+            "|  /      |      \\  |",
+            "| /       |       \\ |",
+            "|/        |        \\|",
+        };
+
+        public Door(int x, int y, int number)
+        {
+            X = x;
+            Y = y;
+            Number = number;
+            Frame = 0;
+        }
+        public void Draw()
+        {
+            Console.CursorTop = y;
+            foreach (string s in Display)
+            {
+                Console.CursorLeft = x;
+                Console.WriteLine(s);
+            }
+
+            Console.CursorLeft = x + DOOR_WIDTH / 2;
+            Console.CursorTop = y + DOOR_HEIGHT / 2;
+            Console.Write(Number);
+
+            Console.CursorLeft = 0;
+            Console.CursorTop = Y + DOOR_HEIGHT;
+        }
+    }
+}
---
+++ FinalProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int screenWidth = Console.WindowWidth;
+            int padding = 2;
+            int doorCount = 3;
+
+            int doorX = screenWidth / 2 - Door.DOOR_WIDTH * doorCount / 2 - padding;
+            int doorY = 2;
+            Door[] doors = new Door[3];
+
+            for (int i = 0; i < doors.Length; i++)
+            {
+                doors[i] = new Door(doorX, doorY, i + 1);
+                doorX += Door.DOOR_WIDTH + padding;
+            }
+
+            foreach (Door d in doors)
+                d.Draw();
+        }
+    }
+}
