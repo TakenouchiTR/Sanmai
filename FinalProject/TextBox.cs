c3324d076b51495e1a0fb4835eeec99a90b613d4
Shawn Carter <totakenouchi@outlook.com>
2020-11-05 13:39:07 -0500

    Add border design options

--- FinalProject/TextBox.cs
+++ FinalProject/TextBox.cs
@@ -6,21 +6,54 @@ namespace FinalProject
 {
     public class TextBox
     {
+        private static char[][] BORDER_PIECES =
+        {
+            new char[] { '│', '─', '┌', '┐', '└', '┘' },
+            new char[] { '┃', '━', '┏', '┓', '┗', '┛' },
+            new char[] { '┊', '┈', '┌', '┐', '└', '┘' },
+            new char[] { '┋', '┉', '┏', '┓', '┗', '┛' },
+            new char[] { '┆', '┄', '┌', '┐', '└', '┘' },
+            new char[] { '┇', '┅', '┏', '┓', '┗', '┛' },
+            new char[] { '║', '═', '╔', '╗', '╚', '╝' }
+        };
+
         int x, y;
         int width, height;
+        BorderType borderType;
 
-        public TextBox(int x, int y, int width, int height)
+        public TextBox(int x, int y, int width, int height, BorderType borderType)
         {
             this.x = x;
             this.y = y;
             this.width = width;
             this.height = height;
+            this.borderType = borderType;
         }
 
         public void WriteText(string text)
         {
             string[] words = text.Split(" ");
-
         }
+    }
+
+    public enum BorderType
+    {
+        SolidLight,
+        SolidHeavy,
+        DottedLight,
+        DottedHeavy,
+        DashedLight,
+        DashedHeavy,
+        DoubleLine
+    }
+
+    enum BorderPiece
+    {
+        Vertical,
+        Horizontal,
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
     }
 }
