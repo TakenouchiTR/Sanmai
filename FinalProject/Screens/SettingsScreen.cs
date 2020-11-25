442b3b6a23c4ce79a624eb2e504bf36adbc3aa0c
Shawn Carter <totakenouchi@outlook.com>
2020-11-25 18:41:24 -0500

    Add setting descriptions and instructions

--- FinalProject/Screens/SettingsScreen.cs
+++ FinalProject/Screens/SettingsScreen.cs
@@ -32,17 +32,34 @@ namespace FinalProject.Screens
             "speed_up_key",
             "speed_down_key"
         };
+        private static readonly string[] DESCRIPTIONS = new string[]
+        {
+            "Changes the door speed; lower numbers are faster",
+            "Toggles whether sounds are muted",
+            "Toggles whether doors should have a chance to open slowly",
+            "The hotkey to mute sounds while in game",
+            "The hotkey to speed up the door while in game",
+            "The hotkey to slow down the door while in game"
+        };
+        private static readonly string[] INSTRUCTIONS = new string[]
+        {
+            "Press the left or right arrow to change the value",
+            "Press enter, then press any key to change the hotkey"
+        };
         private static readonly Regex VALID_CHARS = new Regex(@"[a-z0-9,.\\\[\]/`*\-]");
+        
         #endregion
 
         #region Fields
         private int index;
+        private UIType prevUIType;
         private Spinner spn_speed;
         private Spinner spn_mute;
         private Spinner spn_slowOpen;
         private CharacterDisplay chr_mute;
         private CharacterDisplay chr_speedUp;
         private CharacterDisplay chr_speedDown;
+        private TextBox textBox;
 
         private UIObject[] elements;
         #endregion
@@ -51,6 +68,7 @@ namespace FinalProject.Screens
         public SettingsScreen()
         {
             index = 0;
+            prevUIType = UIType.Spinner;
 
             spn_speed = new Spinner(0, 0, new string[]
             {
@@ -85,6 +103,8 @@ namespace FinalProject.Screens
 
             elements[0].Toggle(false);
 
+            textBox = new TextBox(Console.WindowWidth / 2 - 30, Console.WindowHeight - 10, 60, 8, BorderType.DashedHeavy);
+
             Console.SetCursorPosition(0, 0);
         }
         #endregion
@@ -102,19 +122,25 @@ namespace FinalProject.Screens
 
         public override bool Play()
         {
-            Painter.Write("Press esc to return to the title screen", Console.WindowWidth / 2 - 20,
-                Console.WindowHeight - 2, ConsoleColor.White);
-
             foreach (UIObject obj in elements)
                 obj.Draw();
 
             for (int i = 0; i < LABELS.Length; i++)
                 Painter.Write(LABELS[i], Console.WindowWidth / 2 - 13, 3 + i * 2, ConsoleColor.White);
 
+            textBox.DrawBorder();
+            textBox.WriteText("Settings", TextAlign.Center);
+            textBox.WriteText("------------", TextAlign.Center);
+            textBox.WriteText(DESCRIPTIONS[index], TextAlign.Center);
+            textBox.WriteText();
+            textBox.WriteText(INSTRUCTIONS[(int)prevUIType], TextAlign.Center);
+            textBox.WriteText("Press esc to return to the title screen", TextAlign.Center);
+
             Console.SetCursorPosition(0, 0);
 
             while (true)
             {
+                Console.SetCursorPosition(0, 0);
                 ConsoleKeyInfo key = Input.GetKey(false);
                 Spinner spn;
 
@@ -125,6 +151,8 @@ namespace FinalProject.Screens
                         elements[index].Toggle();
                         index = (index + 1) % elements.Length;
                         elements[index].Toggle();
+
+                        UpdateTextBox();
                         break;
 
                     //Selects the next item up
@@ -132,6 +160,8 @@ namespace FinalProject.Screens
                         elements[index].Toggle();
                         index = index == 0 ? elements.Length - 1 : index - 1;
                         elements[index].Toggle();
+
+                        UpdateTextBox();
                         break;
 
                     //If the player selects a Character Display, allow them to enter a new character
@@ -188,5 +218,48 @@ namespace FinalProject.Screens
             }
         }
         #endregion;
+
+        #region Draw Methods
+        /// <summary>
+        /// Updates the text in the text box whenever a new item is selected
+        /// </summary>
+        private void UpdateTextBox() 
+        {
+            UIType newType;
+
+            //Gets the type of the currently selected element
+            if (elements[index] is Spinner)
+                newType = UIType.Spinner;
+            else
+                newType = UIType.CharacterDisplay;
+
+            //Writes the description for the current setting
+            textBox.ChangeLine(2);
+            textBox.ClearLine();
+            textBox.WriteText(DESCRIPTIONS[index], TextAlign.Center);
+
+            //Updates the instructions if the element type changed
+            if (newType != prevUIType) 
+            {
+                textBox.ChangeLine(4);
+                textBox.ClearLine();
+                textBox.WriteText(INSTRUCTIONS[(int)newType], TextAlign.Center);
+                prevUIType = newType;
+            }
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+
+            textBox.ClearText();
+        }
+        #endregion
+    }
+
+    enum UIType
+    {
+        Spinner,
+        CharacterDisplay
     }
 }
